78e8f4c18305bc0aad8636d5aaaaaa699f5f7f2c
Szendrényi Ákos <szendrenyiakos@gmail.com>
2018-10-09 13:02:45 +0200

    Feladat 1

--- Verziokezeles/Program.cs
+++ Verziokezeles/Program.cs
@@ -10,7 +10,26 @@ namespace Verziokezeles
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kérem adjón meg 10 számót");
+            int[] a = new int[10];
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("Kérem a {0} ez a számót",i+1);
+                a[i] = Convert.ToInt32(Console.ReadLine());
 
+
+            }
+            int b=a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > b)
+                {
+                    b = a[i];
+                } 
+                
+            }
+            Console.WriteLine("A legnagyobb szám {0}",b);
+            Console.ReadKey();
         }
     }
 }
